d040fa2728c02f8024a29b26b0d8284bac8cdfa0
Leandro Steffens de Oliveira <lenhn1007mt@gmail.com>
2021-12-26 19:04:26 -0400

    protótipo

--- projeto.c
+++ projeto.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 
+
+//Declarando as varáveis globais.
 int qnt_box, qnt_truck;
 
 int scan_values();
+
+int choose_box
 
 int main(){
 
     printf("\n\t\tPROJETO TRANSPORTADORA\n\n\n\n");
         for(int i = 0; i < 2000000000; i++)
             printf("");
-    system("pause");
     system("cls");
 
     scan_values();
+
+    choose_box();
 
 return 0;
 }
@@ -22,28 +27,34 @@ int scan_values(){
 
     //Recebendo a quantidade e peso das caixas.
     printf("Digite a quantidade de caixas: ");
-    scanf("%i", &qnt_box);
+        scanf("%i", &qnt_box);
     int tamb = qnt_box - 1;
     int mass_box[tamb];
     int j = 1;
     
     for(int i = 0; i <= tamb; i++){
         printf("\nDigite o peso da caixa %i: ", j++);
-        scanf("%i", &mass_box[i]);
+            scanf("%i", &mass_box[i]);
     }
     system("cls");
     
 
     //Recebendo a quantidade e limite de carga dos caminhões.
     printf("Digite a quantidade de caminhoes: ");
-    scanf("%i", &qnt_truck);
+        scanf("%i", &qnt_truck);
     int tamt = qnt_truck - 1;
     int limit_truck[tamt];
     j = 1;
     
     for(int i = 0; i <= tamt; i++){
         printf("\nDigite o limite de carga do caminhao %i: ", j++);
-        scanf("%i", &limit_truck[i]);
+            scanf("%i", &limit_truck[i]);
     }
+
 }
 
+int choose_box(){
+
+
+
+}
